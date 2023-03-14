2338728c857dcaf18bec23622f6836aac9aa328d
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-15 00:53:33 +1300

    Update README. Create skeleton for project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# rock_paper_scissors
+# rock-paper-scissors
 Rock Paper Scissors Project for TOP
+
+The purpose of this project is to demonstrate and practise javascript. It will initially be played from the browser console. A GUI will be added later. 
---
+++ javascript.js
@@ -0,0 +1,27 @@
+//Rock Paper Scissors
+
+//Get random computer choice, return 'Rock', 'Paper', or 'Scissors'
+    //use console to check this is working correctly
+function getComputerChoice() {
+
+}
+
+//Play single round 
+    //takes two parameters- playerSelection, computerSelection
+    //make playerSelection case insensitive
+    //return string that declares winner "paper beats rock"
+function playRound(playerSelection, computerSelection) {
+
+}
+
+//test functions so far before moving on 
+console.log(playRound("rock"), getComputerChoice());
+
+//create function called game() 
+    //call playRound 5 times and keep score
+function game() {
+    for (let i = 0; i < 5; i++) {
+        //use prompt() to get input from the user 
+    }
+}
+//console.log() to display these results
