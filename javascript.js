75baa11c7632508aa2dcbe9a56ebab499153e510
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-03-15 01:32:39 +1300

    Complete and test getComputerChoice() and playRound()

--- javascript.js
+++ javascript.js
@@ -1,21 +1,38 @@
 //Rock Paper Scissors
 
 //Get random computer choice, return 'Rock', 'Paper', or 'Scissors'
-    //use console to check this is working correctly
 function getComputerChoice() {
-
+    randomChoice = Math.floor(Math.random()*3);
+    switch(randomChoice) {
+        case 0: 
+            return "Rock";
+            break;
+        case 1: 
+            return "Paper";
+            break;
+        case 2: 
+            return "Scissors";
+        default:
+            return "something went wrong";
+    }
 }
 
 //Play single round 
-    //takes two parameters- playerSelection, computerSelection
-    //make playerSelection case insensitive
-    //return string that declares winner "paper beats rock"
 function playRound(playerSelection, computerSelection) {
-
+    //Player Selection is case insensitive. Capitalize only first letter.
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+    //Winning conditions and return strings
+    if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+        return `Player wins! ${playerSelection} beats ${computerSelection}!`;
+    } else if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else {
+        return `Computer wins! ${computerSelection} beats ${playerSelection}!`;
+    }
 }
 
 //test functions so far before moving on 
-console.log(playRound("rock"), getComputerChoice());
+console.log(playRound("rock", getComputerChoice()));
 
 //create function called game() 
     //call playRound 5 times and keep score
